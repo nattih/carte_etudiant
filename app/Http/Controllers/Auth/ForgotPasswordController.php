a42b1655a981860023b6d07284e55fa1fab168d3
Mande Hamed <mandson1er@gmail.com>
2020-07-15 15:33:56 +0000

    Assemblage

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 
+
 class ForgotPasswordController extends Controller
 {
     /*
@@ -19,6 +20,14 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+    public function showLinkRequestForm()
+    {
+        return view('auth.passwords.email');
+        $this->validate($request, ['email' => 'required|email']);
+    
+    }
+
+
 
     /**
      * Create a new controller instance.
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('accueil', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/1', 'HomeController@index')->name('home');
+
+
+/////////////////////////////////////////////////////////////
+
+// Route pour authentification et accueil
+
+// Réservé aux utilisateurs authentifiés
+
+Route::get('/','AuthentificationController@index') ;
+
+
+Route::post('/','AuthentificationController@index1')->name('connection') ;
+    
+Route::get('accueil','AuthentificationController@accueil')->name('accueil') ;
+
+Route::get('ajouter','NouveauController@index')->name('ajouter');
+
+Route::get('lister','ListeController@index')->name('lister');
+Route::get('reimprimer','ReimprimerController@index')->name('reimprimer');
+
+ 
+
+// Route::get('{n}',function($n){ return view($n); })
+
